63c0b96e690e78004c904d8772b067bc3a9d28ad
kyleuniversities <kyleuniversities@gmail.com>
2025-01-29 08:48:59 -0800

    added grid elements

--- client/page/home/HomePage.tsx
+++ client/page/home/HomePage.tsx
@@ -7,6 +7,8 @@ import SidePadder from "@/component/util/spacer/SidePadder";
 import SearchMenu from "./search-menu/SearchMenu";
 import TopNavMenu from "./top-nav-menu/TopNavMenu";
 import ProfileDropdown from "./profile-dropdown/ProfileDropdown";
+import GridContainer from "@/component/util/grid/GridContainer";
+import GridComponent from "@/component/util/grid/GridComponent";
 
 // Parameters Interface
 interface HomePageParameters {
@@ -36,8 +38,19 @@ export default function $moduleName({
         <div className={`${styles["main-container"]} flex-col w-full h-full`}>
           <div className={`${styles["upper-container"]} flex w-full`}>
             <div className={`${styles["action-menu"]} flex-center`}>
-              <div className={`${styles["search-bar"]} flex`}>
-                <SearchMenu />
+              <div className={`${styles["search-bar"]} flex-col`}>
+                <GridContainer>
+                  <GridComponent zIndex={0}>
+                    <SearchMenu />
+                  </GridComponent>
+                  <GridComponent position="absolute" zIndex={0}>
+                    <div style={{ opacity: 0 }}>
+                      <SearchMenu />
+                    </div>
+                    <SearchMenu />
+                    <SearchMenu />
+                  </GridComponent>
+                </GridContainer>
               </div>
               <SidePadder size={10} />
               <TopNavMenu />
